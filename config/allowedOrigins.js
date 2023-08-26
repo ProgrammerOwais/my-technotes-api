7e0157a0734822ab85cbd90d6240ff002c140174
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2023-08-26 16:45:01 +0500

    new changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5000",
   "http://localhost:3000",
-  "https://my-technotes-api.onrender.com",
+  "https://my-technotes-api.vercel.com",
 ];
 
 module.exports = allowedOrigins;
